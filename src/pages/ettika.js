971d02c3e6f63d20d70faf92e4ff2f74d9101771
John Hansen <john.l.hansen@gmail.com>
2019-05-28 11:05:28 -0700

    add form and about me

---
+++ src/pages/ettika.js
@@ -0,0 +1,227 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Hero from '../components/hero'
+import SEO from '../components/seo'
+
+import Header from '../components/header'
+
+import cover from '../images/hero-actv8me-dashboard.png'
+
+const SecondPage = () => (
+  <>
+    <Header />
+    <Hero
+      title="ACTV8ME DASHBOARD"
+      image={require('../images/hero-actv8me-dashboard.png')}
+    />
+
+    <Layout>
+      <SEO title="Page two" />
+      <div
+        style={{
+          maxWidth: `540px`,
+          margin: `0 auto`,
+        }}
+      >
+        <h2
+          style={{
+            marginTop: `6rem`,
+          }}
+        >
+          About
+        </h2>
+        <p
+          style={{
+            maxWidth: `540px`,
+            margin: `0 auto`,
+            lineHeight: `1.6rem`,
+          }}
+        >
+          I helped the design and engineering team redesign the dashboard to
+          improve the user experience, optimize existing features, and do so in
+          a way that lays the foundation for future features. Working closely
+          with the VP of Design, content creators and stakeholders, I collected
+          and refined requirements, designed and prototyped solutions, created
+          actionable tickets and followed them through to production.
+        </p>
+
+        <ul
+          style={{
+            listStyle: `none`,
+            paddingLeft: `0`,
+            marginLeft: `0`, 
+            marginTop: `4rem`,
+            lineHeight: `1.5rem`,
+            textTransform: `uppercase`,
+          }}
+        >
+          <li>
+            <strong>versioning:</strong> abstract + github{' '}
+          </li>
+          <li>
+            <strong>prototype:</strong> sketch + invision studio
+          </li>
+          <li>
+            <strong>hand-off:</strong> invision
+          </li>
+          <li>
+            <strong>code:</strong> angular material
+          </li>
+        </ul> 
+
+        <h2
+          style={{
+            marginTop: `4rem`,
+          }}
+        >
+          Process
+        </h2>
+
+        <h3
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
+          Organize   Requirements
+        </h3>
+
+        <p
+          style={{
+            maxWidth: `540px`,
+            margin: `0 auto`,
+            lineHeight: `1.6rem`,
+          }}
+        >
+          Business wrote documentation on what they wanted and Engineering
+          described what was currently implemented while Design had
+          documentation sort of in between the two, including additional desired
+          features. I took them all and identified what was implemented and what
+          was needed.
+        </p>
+
+        <h3
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
+          User Inquiries
+        </h3>
+
+        <p
+          style={{
+            maxWidth: `540px`,
+            margin: `0 auto`,
+            lineHeight: `1.6rem`,
+          }}
+        >
+          After knowing what was needed, I proceeded to ask questions and
+          observe current users of the product to identify pain points. A few
+          quickly surfaced - this gave my what was required for the project and
+          places where I could improve User Experience.
+        </p>
+
+        <h3
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
+          User Flows
+        </h3>
+
+        <p
+          style={{
+            maxWidth: `540px`,
+            margin: `0 auto`,
+            lineHeight: `1.6rem`,
+          }}
+        >
+          Creating user flows of features to visualize my new design
+          ideas helped me check myself and was also extremely valuable as
+          a sort of glimpse-map in future tickets for developers.
+        </p>
+
+        <h3
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
+          Protopying
+        </h3>
+
+        <p
+          style={{
+            maxWidth: `540px`,
+            margin: `0 auto`,
+            lineHeight: `1.6rem`,
+          }}
+        >
+          User flows transformed into wireframes that I prototyped, using them
+          to communicate my decisions in action to the design team. This was also
+          another, <em>more intimate</em>, check on the logic and smoothness of the flow.
+        </p>
+
+        <h3
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
+          Visual Design
+        </h3>
+
+        <p
+          style={{
+            maxWidth: `540px`,
+            margin: `0 auto`,
+            lineHeight: `1.6rem`,
+          }}
+        >
+        Create and apply color palette, typography and spatial relationships. To check again if I was suitably solving some of the pain points mentioned in user research, I refined prototypes and got feedback from the content team.
+        </p>
+
+        <h3
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
+          Implementation
+        </h3>
+     
+        <p
+          style={{
+            maxWidth: `540px`,
+            margin: `0 auto`,
+            lineHeight: `1.6rem`,
+          }}
+        >
+        Clean up, prepare and organize flows in InVision, create tickets and attach the two together and hand-off. Responsible for making sure design is implemented as intended and work with developers to get features to Production. 
+        </p>
+
+          
+        <h2
+          style={{
+            marginTop: `4rem`,
+          }}
+        >
+          Takeaways
+        </h2>
+        <p
+          style={{
+            maxWidth: `540px`,
+            margin: `0 auto`,
+            lineHeight: `1.6rem`,
+          }}
+        >
+          Furthering my knowledge and implementing of Sketch components,
+          overrides and styles. On a level, it felt like I was designing for a
+          designer and that was peculiar and refreshing to me.
+        </p>
+
+        <Link to="/">Back</Link>
+      </div>
+    </Layout>
+  </>
+)
+
+export default SecondPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,23 +20,85 @@ const IndexPage = props => (
         marginTop: `5rem`,
       }}
     >
-      <Link to="/page-2/">
+      <Link to="/mobii/">
         <Img fluid={props.data.imageOne.childImageSharp.fluid} />
       </Link>
+
+      <Link to="/actv8me-dashboard">
       <Img fluid={props.data.imageTwo.childImageSharp.fluid} />
+      </Link>
+
+      <Link to="/actv8me-branding/">
       <Img fluid={props.data.imageThree.childImageSharp.fluid} />
+      </Link>
+
+      <Link to="/ettika/">
       <Img fluid={props.data.imageFour.childImageSharp.fluid} />
+      </Link>
+
+      <Link to="/oubly-dashbaord/">
       <Img fluid={props.data.imageFive.childImageSharp.fluid} />
+      </Link>
+
+      <Link to="/oubly-product">
       <Img fluid={props.data.imageSix.childImageSharp.fluid} />
+      </Link>
+
     </div>
     <div style={{
       marginTop: `5rem`,
     }}>
-    <h1>Hi people</h1>
-    <p>It all started when I was young.</p>
+    <h1>About Me</h1>
+
+    <p>I love to draw. in school it was a challenge for me to focus on the current topic and not draw. I used to sell and trade posters, comics and band logos I'd make. I also love computers. in elementary school I became a minor celebrity for making pixel art wallpapers. When I got my first computer I needed room to install a tank game (the PC was very limited as it was a basic business machine) so I started digging around in DOS. I loved how you could manipulate the machine through textual commands, a fascination which would reignite later when I would discover HTML, CSS and JavaScript. I knew I wanted to be in art and technology somehow.</p>
+
+    <p>
+    However, I struggled with how to get there; I was not very good at nor did I enjoy the traditional art classes I was taking. I started floundering in high school and lost hope on pursuing art. Looking for direction and always having had a desire to join the military I enlisted in the Navy. Doing well in the Navy I came to a crossroads: to stay in and go for a commission or leave and utilize the GI Bill for art school - I chose the latter.
+    </p>
+
+    <p>
+    Have a passion for gaming, I initially started studying game design. One of my classmates that I often studied with told me about her program, graphic design. I asked about it and she told me how it was about branding, typography, web and all the goodness. My heart leapt, I didn't know this was a thing. I immediately changed my focus to design and it was exactly what I'd been looking for.
+    </p>
+
+    <p>
+    After school I joined a startup and haven't stopped since! My mind is continually blown by this constantly evolving field. When not designing, I'll probably be gaming, cooking or playing guitar.
+    </p>
+
+    <h2>
+    Tool Set
+    </h2>
+
+    <p>
+    Although I dabble with myriad tools, I find myself using the following most:
+    </p>
+
+    <h2>Contact</h2>
+
+    <p>
+    Feel free to reach out for any reason.
+    </p>
+
+    <form name="contact" netlify>
+
+    <p>
+    <label>Name <input type="text" name="name" /></label>
+    </p>
+
+    <p>
+    <label>Email <input type="text" name="email" /></label>
+    </p>
+
+    <p>
+    <label>Message <input type="textarea" cols="1" rows="5" name="message" /></label>
+    </p>
+
+    <button type="submit">Send</button> 
+
+    </form>
+
 
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }} />
-    <Link to="/page-2/">Link Example</Link>
+    <Link to="/mobii/">Link Example</Link>
     </div>
   </Layout>
   </>
