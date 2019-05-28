da816291a2d36173275ed8cc025ef85af8e0c6d9
John Hansen <john.l.hansen@gmail.com>
2019-05-28 11:18:16 -0700

    update form

--- src/pages/index.js
+++ src/pages/index.js
@@ -78,23 +78,26 @@ const IndexPage = props => (
     Feel free to reach out for any reason.
     </p>
 
-    <form name="contact" netlify>
-
-    <p>
-    <label>Name <input type="text" name="name" /></label>
-    </p>
-
-    <p>
-    <label>Email <input type="text" name="email" /></label>
-    </p>
-
-    <p>
-    <label>Message <input type="textarea" cols="1" rows="5" name="message" /></label>
-    </p>
-
-    <button type="submit">Send</button> 
-
-    </form>
+    <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
 
 
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }} />
